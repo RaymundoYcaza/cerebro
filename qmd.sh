a74785c2276d29dd4925d8d0d8cc8c59dfe8bbce
Raymundo Ycaza <raymundo.ycaza.m@gmail.com>
2026-04-05 01:18:39 -0500

    Comenta las salidas por consola de qmd.sh

--- qmd.sh
+++ qmd.sh
@@ -4,8 +4,8 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 export XDG_CACHE_HOME="$DIR/qmd/.cache"
 
-echo "Running from:"
-echo "$DIR/qmd/bin/qmd"
+#echo "Running from:"
+#echo "$DIR/qmd/bin/qmd"
 
 "$DIR/qmd/bin/qmd" \
   --index portable \
